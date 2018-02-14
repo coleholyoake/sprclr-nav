27aa3aa9d93faaa0c2a94e78e47d541e23e5f2b5
Cole Holyoake <coleholyoake@live.com>
2018-02-14 15:09:13 +1300

    Messy initial code

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+// $('.dropdown-toggle').hover(function() {
+//   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+// }, function() {
+//   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+// });
+
+//
+// $('.parent > *')
+//     .focus(function() {
+//         $('.parent').addClass('focused');
+//     })
+//     .blur(function() {
+//         $('.parent').removeClass('focused');
+//     });
+
+// var myDropDowns = $( ".dropdown-toggle" );
+//
+// $(myDropDowns).click(function() {
+//   event.stopPropagation();
+//   myDropDowns.parent().removeClass('open')
+//   $(this).parents('.dropdown, .dropdown-submenu').addClass('open');
+// });
